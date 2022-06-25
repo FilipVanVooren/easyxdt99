ecb8051528b0e85b51cd6800f7bcd9efd8cc911c
Filip van Vooren <filip.van-vooren@oratronik.de>
2022-06-25 15:12:58 +0200

    Add  bash to alpine

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-version=${1:-3.5.0}
+xdtversion=${1:-3.5.0}
 pyimage=${2:-library/python}
 pyversion=${3:-3.10}
+easyxdt=${4:-1.10}
 
 if [[ $pyimage == *"/python"* ]]; then
   pyinterp="cpython"
@@ -17,7 +18,7 @@ fi
 echo "Prepare building docker image with XDT99-${version} on Python ${pyversion}"
 echo "Building from \"${pyimage}\" image"
 
-docker build --build-arg VERSION="${version}"     \
-             --build-arg PYIMAGE="${pyimage}"     \
-             --build-arg PYVERSION="${pyversion}" \
-             --tag "easyxdt99:${version}-${pyinterp}${pyversion}" .
+docker build --build-arg XDTVERSION="${xdtversion}"  \
+             --build-arg PYIMAGE="${pyimage}"        \
+             --build-arg PYVERSION="${pyversion}"   \
+             --tag "easyxdt99:${xdtversion}-${pyinterp}${pyversion}" .
--- build/build_easyxdt99_cpython_alpine.sh
+++ build/build_easyxdt99_cpython_alpine.sh
@@ -4,16 +4,6 @@ export PATH=.:$PATH
 
 pyver=${1:-3.10}
 
-# Build XDT99 3.X series on CPython 3.X image
-build.sh 3.5.0 library/python "$pyver"
-build.sh 3.4.0 library/python "$pyver"
-build.sh 3.3.2 library/python "$pyver"
-build.sh 3.3.1 library/python "$pyver"
-build.sh 3.3.0 library/python "$pyver"
-build.sh 3.2.0 library/python "$pyver"
-build.sh 3.1.0 library/python "$pyver"
-build.sh 3.0.0 library/python "$pyver"
-
 # Build XDT99 3.X series on CPython 3.X-alpine image
 build.sh 3.5.0 library/python "$pyver-alpine"
 build.sh 3.4.0 library/python "$pyver-alpine"
