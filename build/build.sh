66a0bd62787a502bbf28074c17a78e4433069691
Filip van Vooren <filip.van-vooren@oratronik.de>
2022-06-26 13:23:22 +0200

    Fix for macos

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 xdtversion=${1:-3.5.0}
 xdt99tar="xdt99-${xdtversion}.tar.gz"
 pyimage=${2:-library/python}
@@ -16,14 +14,31 @@ else
     exit 1
 fi
 
-echo "Check xdt99 source package"
-mkdir -p filecache
+mkdir -p .filecache
+
+echo "Check xdt99 source package: ${xdt99tar}"
 if [[ -f  "./.filecache/${xdt99tar}" ]]; then
   echo "xdt99 source package already cached"
+
 else
-  echo "Getting xdt99 source package from github"
-  wget -P .filecache \
+  exist_wget="$(which wget)"
+  exist_curl="$(which curl)"
+
+  if [ "${#exist_wget}" -gt "0" ]; then
+    echo "Getting xdt99 source package from github using wget"
+    wget -P .filecache \
       "https://github.com/endlos99/xdt99/releases/download/${xdtversion}/${xdt99tar}"
+
+  elif [ "${#exist_curl}" -gt "0" ]; then
+    echo "Getting xdt99 source package from github using curl"
+    curl -s -o ".filecache/${xdt99tar}" \
+      "https://github.com/endlos99/xdt99/releases/download/${xdtversion}/${xdt99tar}"
+
+  else
+    echo "Not found"
+    exit 255
+  fi
+
 fi
 
 echo "Prepare building docker image with XDT99-${xdtversion} on Python ${pyversion}"
